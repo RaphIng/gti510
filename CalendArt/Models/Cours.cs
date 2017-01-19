06986636bf644939e9e3c2be8fc37e01f0b7cc6d
'Vincent <'AM55720@ens.etsmtl.ca'>
2017-01-18 19:43:41 -0500

    Création de 3 classes Model

---
+++ CalendArt/Models/Cours.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalendArt.Models
+{
+    public class Cours
+    {
+        public int Id { get; set; }
+        public string Sigle { get; set; }
+        public string Titre { get; set; }
+        public string Description { get; set; }
+        public string Professeur { get; set; }
+    }
+}
---
+++ CalendArt/Models/Tache.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalendArt.Models
+{
+    public class Tache
+    {
+        public int Id { get; set; }
+        public string Titre { get; set; }
+        //public int TypeId { } // devoir, quiz/test, examen, etc.. ** avec enum? comment faire avec bonne pratique? - Vincent **
+        public string Description { get; set; }
+        public DateTime DateHeure { get; set; }
+        public int Ponderation { get; set; }
+    }
+}
