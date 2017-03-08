dbce4685e18ecd1f26d45a17ff49dde9cd14dcfd
Vincent <AM55720@ens.etsmtl.ca>
2017-03-08 13:41:56 -0500

    Resolve some merge conflicts

---
+++ CalendArt/Core/Repositories/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace CalendArt.Core.Repositories
+{
+    // interface pour repository pattern
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+        void AddList(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+        void RemoveList(IEnumerable<TEntity> entities);
+    }
+}
