558b5a3e0161487bfbe248e0f23a22391e52404c
awadds <amine.waddah@gmail.com>
2017-03-20 20:21:54 -0400

    return clientID and ClientSecret

--- CalendArt/App_Start/ClientSecrets.cs
+++ CalendArt/App_Start/ClientSecrets.cs
@@ -5,7 +5,9 @@ using System.Web;
 
 namespace CalendArt.App_Start
 {
-    public class ClientSecrets
+    internal static class ClientSecrets
     {
+        public const string ClientId = "348647408423-ojqt0q9mccpvlc00rvijpa7nvfktb153.apps.googleusercontent.com";
+        public const string ClientSecret = "VGVl6XH8ut_qSeX476JtfO1P";
     }
 }
